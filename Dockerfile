81d603de4575802449791e847d0d8f05d15ed0c5
Rafli <raflisatriandi@outlook.co.id>
2022-05-13 14:43:29 +0700

    unfinished work

--- Dockerfile
+++ Dockerfile
@@ -6,16 +6,6 @@ COPY . /app
 
 WORKDIR /app
 
-# server configuration
-ENV PORT=8000
-
-# postgres configuration
-ENV PG_HOST=192.168.18.240
-ENV PG_PORT=5432
-ENV PG_USER=postgres
-ENV PG_PASS=Raflis2001
-ENV PG_DB=monarch-thesis
-
 RUN go mod verify
 
 RUN go mod tidy
--- src/controller/controller_global_var.go
+++ src/controller/controller_global_var.go
@@ -2,6 +2,13 @@ package controller
 
 import "github.com/ara-thesis/monarch-project-be/src/helper"
 
+type minioCred struct {
+	endpoint        string
+	accessKeyId     string
+	secretAccessKey string
+	useSSL          bool
+}
+
 var (
 	tbname = map[string]string{
 		"news":             "newstb",
@@ -10,7 +17,13 @@ var (
 		"account_roleinfo": "roleinfo",
 		"placeinfo":        "placeinfotb",
 	}
-	db        = new(helper.PgHelper)
-	resp      = new(helper.ResponseHelper)
-	jwthelper = new(helper.JwtHelper)
+	db               = new(helper.PgHelper)
+	resp             = new(helper.ResponseHelper)
+	jwthelper        = new(helper.JwtHelper)
+	miniocredentials = minioCred{
+		"localhost:9000",
+		"YV2n9zZ1rSqDbNZF",
+		"V1ynQ8Xx6RTMsHx9rDTZFNp61pdWDHFM",
+		false,
+	}
 )
